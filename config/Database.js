d4f0863d231ba7a1b44b332f401d83baaa029120
Saidil Mifdal <saidilmifdal15@gmail.com>
2023-11-05 16:45:56 +0800

    deploy

--- config/Database.js
+++ config/Database.js
@@ -1,7 +1,11 @@
 import { Sequelize } from "sequelize";
-const db = new Sequelize("crud_db", "root", "", {
-  host: "localhost",
-  dialect: "mysql",
+const db = new Sequelize({
+  host: process.env.DB_HOST,
+  user: process.env.DB_USERNAME,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_DBNAME,
+  dialect: "postgres",
+  logging: false,
 });
 
 export default db;
